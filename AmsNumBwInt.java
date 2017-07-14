078b4ed30e20eecce70a0a3b86e510f056a40b57
VLokeshKumar <loki631995@gmail.com>
2017-07-14 15:52:10 +0530

    Create AmsNumBwInt.java

---
+++ AmsNumBwInt.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class AmsNumBwInt 
+{	
+   public static void main(String args[])
+   {
+	Scanner sc=new Scanner(System.in);
+	int low, high, i, temp1, temp2, remainder, n = 0, result = 0;
+	System.out.println("Enter two numbers(intervals): ");
+	low=sc.nextInt();
+	high=sc.nextInt();
+	System.out.println("Armstrong numbers between Given number are: ");
+
+	 for(i = low + 1; i < high; ++i)
+	 {
+	  temp2 = i;
+	  temp1 = i;
+	  while (temp1 != 0)
+	  {
+	    temp1 /= 10;
+	    ++n;
+	  }
+      while (temp2 != 0)
+	  {
+	    remainder = temp2 % 10;
+	    result += Math.pow(remainder, n);
+	    temp2 /= 10;
+	   }
+	   if (result == i) 
+	   {
+	     System.out.println(i);
+	   }
+	   n=0;
+	   result = 0;
+	   }
+	}
+}
